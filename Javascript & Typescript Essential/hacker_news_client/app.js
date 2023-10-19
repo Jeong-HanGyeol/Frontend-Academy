0005df050b9777bf56ef3e3ac1ff6fae17f87089
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-10-19 14:29:47 +0900

    라우터? 화면 처리기 만들기

--- Javascript & Typescript Essential/hacker_news_client/app.js
+++ Javascript & Typescript Essential/hacker_news_client/app.js
@@ -1,6 +1,5 @@
 const container = document.getElementById("root");
 const ajax = new XMLHttpRequest(); //상수 변하지 않음
-const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
@@ -11,33 +10,50 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener("hashchange", function (event) {
-  const id = location.hash.substr(1);
+  newsList.push("<ul>");
 
-  const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement("h1");
-
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-  console.log(newsContent);
-});
-
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-
-  div.innerHTML = `
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
     <li>
       <a href="#${newsFeed[i].id}">
         ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>
-  `;
+  `);
+  }
 
-  ul.appendChild(div.firstElementChild);
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
