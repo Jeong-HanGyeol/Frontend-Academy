659532e78605a7ba9834381d9078060c7fc80cf7
Jeong-HanGyeol <gksruf05121@gmail.com>
2023-10-20 12:38:44 +0900

    복잡한 UI 구현을 위한 준비 작업 - 템플릿

--- Javascript & Typescript Essential/hacker_news_client/app.js
+++ Javascript & Typescript Essential/hacker_news_client/app.js
@@ -16,8 +16,18 @@ function getData(url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
-
-  newsList.push("<ul>");
+  let template = `
+    <div class="container mx-auto p-4">
+      <h1>Hacker News</h1>
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `;
 
   for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
@@ -29,18 +39,14 @@ function newsFeed() {
   `);
   }
 
-  newsList.push("</ul>");
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+  template = template.replace(
+    "{{__prev_page__}}",
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace("{{__next_page__}}", store.currentPage + 1);
 
-  newsList.push(`
-    <div>
-      <a href="#/page/${
-        store.currentPage > 1 ? store.currentPage - 1 : 1
-      }">이전 페이지</a>
-      <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
-    </div>
-  `);
-
-  container.innerHTML = newsList.join("");
+  container.innerHTML = template;
 }
 
 function newsDetail() {
